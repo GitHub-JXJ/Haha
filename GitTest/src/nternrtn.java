7cda13c01709726fcaebb350622c1d6d5128788a
Jxj <973282189@qq.com>
2017-03-17 09:59:54 +0800

    Update nternrtn.java

--- GitTest/src/nternrtn.java
+++ GitTest/src/nternrtn.java
@@ -3,5 +3,6 @@ public class nternrtn {
     public static final int TYPE = 0;
     public static final int VALUE = 1;
     public static final String NAME = "Jack";
-    public static final String GIRL = "Kitty";
+    public static final String GIRL_OLD = "Kitty6666666666";
+    public static final String GIRL_NEW = "Kitty888888888888";
 }
